3b3c020e40b662f22081af876c1207acdc399f8e
Shura_Windows <shura.5044@gmail.com>
2026-04-01 13:18:52 +0300

    BagFix дребезга контактов, Fix отмены таймера если вентилятор не включился.  Добавил таймер на переход микроконтроллера в спящий режим.

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,14 @@ const unsigned long DELAY_STOP_MOTOR = 1000UL;      // * 1000 * 60;
 const unsigned long MAX_TIME_WORK_FAN = 5000UL;     // * 1000 * 60;
 const unsigned long MAX_TIME_PAUZA_FAN = 3000UL; 
 const unsigned long DELAY_SWITCH_CHATTER = 10UL;
+const unsigned long DELAY_STENDBY_SLEEP = 50000UL;
 
 volatile unsigned long zeroTime = 0;
 volatile bool zeroTriggerOn = false;
 bool zeroTriggerOff = false;
 unsigned long impulsDelay = 0;
 
+bool switchRoom = false;
 bool switchChatterEnable = false;
 bool switchChatterDelay = false;
 bool oldStateSwitch = false;
@@ -47,6 +49,7 @@ Timer timerFan;
 Timer timerMaxWorkFan;
 Timer timerPauzaFan;
 Timer timerSwitchChatter;
+Timer timerStandBySleep;
 
 //Функция обработки прерывания
 void zeroTriggerISR() {
@@ -106,14 +109,9 @@ void chatterContact() {
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_25, HIGH);
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_26, HIGH);
   //Активировать Переменную от любой включеной кнопки
-  bool switchRoom = digitalRead(restRoomPin) || digitalRead(bathRoomPin);
-  //Опрос состояния контактов
-  if (switchRoom != oldStateSwitch) {
-    oldStateSwitch = switchRoom;
-    switchChatterDelay = true;//Активировать таймер
-  }
+  switchRoom = digitalRead(restRoomPin) || digitalRead(bathRoomPin);
   //Запустить таймен задержки дребезга
-  if (timerSwitchChatter.check(switchChatterDelay, DELAY_SWITCH_CHATTER)) {
+  if (timerSwitchChatter.check(switchRoom != oldStateSwitch, DELAY_SWITCH_CHATTER)) {
     if (switchRoom) {
       if (Fan) {
         TimerMode = OnFan;
@@ -127,9 +125,10 @@ void chatterContact() {
       TimerMode = DelaySpeedChange;
     }
     else {
+      TimerMode = StandBy;
       blokFan = false;
     }
-    switchChatterDelay = false;//Сбросить таймер
+    oldStateSwitch = switchRoom;
   }
 }
 
@@ -157,7 +156,9 @@ void fanControl(int Mode) {
   //
   switch (TimerMode) {
     case StandBy:
-      esp_light_sleep_start();
+      if (timerStandBySleep.check(!switchRoom, DELAY_STENDBY_SLEEP)) {
+        esp_light_sleep_start();
+      }
       break;
     case OnFan:
       DelayTimerFan = DELAY_RESTART_MOTOR;
@@ -221,8 +222,8 @@ void loop() {
   chatterContact();
   fanControl(1);
   simistorControl();
-  // Serial.print(TimerMode);
-  // Serial.print(" ");
-  // Serial.println(blokFan);
+  Serial.print(TimerMode);
+  Serial.print(" ");
+  Serial.println(blokFan);
 }
 
