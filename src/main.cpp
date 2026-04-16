1f379bdc8c780b5998b366a943943a47514d8ca1
Shura_Windows <shura.5044@gmail.com>
2026-04-16 14:04:37 +0300

    Прописан автоматический выбор библиотеки wifi в зависимости от подключенного контроллера.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include <Timer.h>
+#if defined(ESP32)
 #include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif
 #include <WiFiUdp.h>
 #include <TelnetStream.h>
 #include <ArduinoOTA.h>
